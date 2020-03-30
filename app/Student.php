d126cedef0a3f301d4323996dac0da5b208e2289
Eimantas <eim.kasp@gmail.com>
2020-03-30 19:01:49 +0300

    Update Student.php

--- app/Student.php
+++ app/Student.php
@@ -12,5 +12,18 @@ class Student extends Model
     {
         return $this->hasMany('App\Grade', 'student_id', 'id');
     }
+    
+    public function gradeAverageByLecture($lectureId) {
+        $gradesByLecture = Grade::where('student_id', '=', $this->id)->where('lecture_id', '=', $lectureId);
+        
+        $average = 0;
+        foreach($gradesByLecture as $grade) {
+            $average += $grade->value;
+        }
+        
+        $average = $average / count($gradesByLecture);
+        
+        return $average;
+    }
 
 }
